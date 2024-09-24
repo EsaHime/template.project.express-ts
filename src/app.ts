494653f2297189bb4628f91b2bfb7956c3a64667
LancerComet <chw644@hotmail.com>
2024-09-24 18:18:00 +0800

    + Add more infrastructure.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,37 @@
 import 'dotenv/config'
+
+import path from 'path'
+import url from 'url'
 import express from 'express'
+import session from 'express-session'
+
+declare module 'express-session' {
+  interface SessionData {
+    // Add properties here.
+  }
+}
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const port = parseInt(process.env.PORT || '5000')
 
 const app = express()
-const port = parseInt(process.env.PORT || '3000')
+app.set('view engine', 'pug')
+app.set('views', path.resolve(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: {
+    secure: false,
+    httpOnly: true
+  }
+}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
